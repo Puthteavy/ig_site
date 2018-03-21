2914ecc1f7d9da44698eb8e487093e97866d449a
DESKTOP-6BC9KLD\Puttheavy Tep <theavytep18@gmail.com>
2018-03-21 17:25:02 +0700

    add

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.toTop').click(function(){
+		$('body,html').animate({scrollTop:0}, 1000);
+		return false;
+	});
+});
+$(function() {
+	$(window).scroll(function () {
+		
+	});
+});
